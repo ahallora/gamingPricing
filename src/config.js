0a14dd4f2d7495bef601bc15b09ff73c5462a7eb
ahallora <ah@allora.dk>
2020-10-29 21:34:41 +0100

    Update config.js
    My build configuration: https://www.userbenchmark.com/UserRun/34454651

--- src/config.js
+++ src/config.js
@@ -2,7 +2,12 @@ module.exports = {
   OUTPUT_FOLDER: "./output",
   LIST_OF_ITEMS_URLS: [
     "https://www.edbpriser.dk/produkt/be-quiet-dark-rock-pro-4/",
-    "https://www.edbpriser.dk/produkt/amd-ryzen-5-3600-3-6-ghz-processor/"
-    //...
+    "https://www.edbpriser.dk/produkt/amd-ryzen-5-3600-3-6-ghz-processor/",
+    "https://www.edbpriser.dk/produkt/asrock-b550-phantom-gaming-4/,
+    "https://www.edbpriser.dk/produkt/gigabyte-geforce-gtx-1660-super-oc-6g/",
+    "https://www.edbpriser.dk/produkt/kingston-a2000-4596818",
+    "https://www.edbpriser.dk/produkt/corsair-cv-series-cv650/",
+    "https://www.edbpriser.dk/produkt/nzxt-h-series-h510/",
+    "https://www.edbpriser.dk/produkt/corsair-vengeance-lpx-4653418/"
   ]
 };
