6fda9ed16e34d6bf999bcda5c05514e68ad54019
ahallora <ah@allora.dk>
2020-10-29 21:35:02 +0100

    Update config.js
    My build configuration: https://www.userbenchmark.com/UserRun/34454651

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   LIST_OF_ITEMS_URLS: [
     "https://www.edbpriser.dk/produkt/be-quiet-dark-rock-pro-4/",
     "https://www.edbpriser.dk/produkt/amd-ryzen-5-3600-3-6-ghz-processor/",
-    "https://www.edbpriser.dk/produkt/asrock-b550-phantom-gaming-4/,
+    "https://www.edbpriser.dk/produkt/asrock-b550-phantom-gaming-4/",
     "https://www.edbpriser.dk/produkt/gigabyte-geforce-gtx-1660-super-oc-6g/",
     "https://www.edbpriser.dk/produkt/kingston-a2000-4596818",
     "https://www.edbpriser.dk/produkt/corsair-cv-series-cv650/",
